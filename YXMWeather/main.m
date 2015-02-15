574076205d1f0a796ba494fe2ef694f78ca8c3cc
XianMingYou <YouXianMing@YouXianMing-3.local>
2015-02-15 16:11:31 +0800

    Initial Commit

---
+++ YXMWeather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YXMWeather
+//
+//  Created by XianMingYou on 15/2/15.
+//  Copyright (c) 2015年 XianMingYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
