5b78087751d17c0bb7d437ad12aba95a66c9973a
XianMingYou <YouXianMing@YouXianMing-3.local>
2015-02-15 16:19:15 +0800

    建立项目

--- YXMWeather/AppDelegate.m
+++ YXMWeather/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    
+    self.window.rootViewController = [ViewController new];
+    
+    
     return YES;
 }
 
--- YXMWeather/ViewController.m
+++ YXMWeather/ViewController.m
@@ -19,9 +19,4 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
