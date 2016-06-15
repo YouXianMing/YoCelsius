f7733f8bb349f6b982ad8292bdbb569ac2452f85
YouXianMing <YouXianMing1987@126.com>
2016-06-15 14:12:26 +0800

    Fix net error.

--- YoCelsius/GetWeatherData.m
+++ YoCelsius/GetWeatherData.m
@@ -13,7 +13,7 @@
 #import "CurrentWeatherData.h"
 #import "Networking.h"
 
-static NSString *appIdKey = @"8781e4ef1c73ff20a180d3d7a42a8c04";
+static NSString *appIdKey = @"8c0e04b52e6da9e67c51a102d6269a60";
 
 typedef enum : NSUInteger {
     
@@ -41,29 +41,30 @@ typedef enum : NSUInteger {
         return;
     }
     
-    NSString *latStr = [NSString stringWithFormat:@"%f", self.location.coordinate.latitude];
-    NSString *lonStr = [NSString stringWithFormat:@"%f", self.location.coordinate.longitude];
+    NSString *baseUrl   = @"http://api.openweathermap.org/data/2.5/weather";
+    NSString *latStr    = [NSString stringWithFormat:@"%f", self.location.coordinate.latitude];
+    NSString *lonStr    = [NSString stringWithFormat:@"%f", self.location.coordinate.longitude];
+    NSString *urlString = [NSString stringWithFormat:@"%@?lat=%@&lon=%@&APPID=%@", baseUrl, latStr, lonStr, appIdKey];
     
     // 请求1
-    self.networkWeather = [Networking getMethodNetworkingWithUrlString:@"http://api.openweathermap.org/data/2.5/weather"
-                                                      requestParameter:@{@"lat"   : latStr,
-                                                                         @"lon"   : lonStr,
-                                                                         @"APPID" : appIdKey}
-                                                       requestBodyType:[HttpBodyType type]
-                                                      responseDataType:[JsonDataType type]];
-    self.networkWeather.tag             = kWeather;
-    self.networkWeather.delegate        = self;
-    self.networkWeather.timeoutInterval = @(8.f);
+    self.networkWeather                  = [Networking new];
+    self.networkWeather.urlString        = urlString;
+    self.networkWeather.tag              = kWeather;
+    self.networkWeather.delegate         = self;
+    self.networkWeather.timeoutInterval  = @(8.f);
+    self.networkWeather.method           = [PostMethod type];
+    self.networkWeather.requestBodyType  = [HttpBodyType type];
+    self.networkWeather.responseDataType = [JsonDataType type];
     [self.networkWeather startRequest];
     
     //  请求2
-    self.networkDaily = [Networking getMethodNetworkingWithUrlString:@"http://api.openweathermap.org/data/2.5/forecast/daily"
-                                                    requestParameter:nil
-                                                     requestBodyType:[HttpBodyType type]
-                                                    responseDataType:[JsonDataType type]];
-    self.networkDaily.tag             = kDaily;
-    self.networkDaily.delegate        = self;
-    self.networkDaily.timeoutInterval = @(8.f);
+    self.networkDaily                  = [Networking new];
+    self.networkDaily.tag              = kDaily;
+    self.networkDaily.delegate         = self;
+    self.networkDaily.timeoutInterval  = @(8.f);
+    self.networkDaily.method           = [PostMethod type];
+    self.networkDaily.requestBodyType  = [HttpBodyType type];
+    self.networkDaily.responseDataType = [JsonDataType type];
 }
 
 - (void)requestSucess:(Networking *)networking data:(id)data {
@@ -74,10 +75,13 @@ typedef enum : NSUInteger {
         CurrentWeatherData *currentData = [[CurrentWeatherData alloc] initWithDictionary:data];
         if (currentData.cod.integerValue == 200) {
             
-            self.currentWeatherData            = currentData;
-            self.networkDaily.requestParameter = @{@"id"    : self.currentWeatherData.cityId,
-                                                   @"cnt"   : @"14",
-                                                   @"APPID" : appIdKey};
+            self.currentWeatherData = currentData;
+            
+            NSString *baseUrl   = @"http://api.openweathermap.org/data/2.5/forecast/daily";
+            NSString *urlString = [NSString stringWithFormat:@"%@?id=%@&cnt=%@&APPID=%@", baseUrl, self.currentWeatherData.cityId, @"14", appIdKey];
+            
+            self.networkDaily.urlString = urlString;
+            
             [self.networkDaily startRequest];
             
         } else {
